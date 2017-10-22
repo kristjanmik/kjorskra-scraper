ac4f55140602b33506ff8f8bd2825fac2591cd78
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2017-10-22 19:53:50 -0400

    Perform direct https requests instead of going through headless chrome

--- index.js
+++ index.js
@@ -1,111 +1,12 @@
 const express = require('express');
-const app = express();
-const { exec } = require('child_process');
-const puppeteer = require('puppeteer');
+const kjorskra = require('./kjorskra');
 
-app.use((req, res, next) => {
-  const allowedHeaders = [
-    'X-Requested-With',
-    'Accept',
-    'Origin',
-    'Referer',
-    'User-Agent',
-    'Content-Type',
-    'Authorization',
-    'X-Mindflash-SessionID'
-  ];
+const PORT = process.env.PORT || 3400;
 
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', allowedHeaders.join(', '));
-
-  if (req.method !== 'OPTIONS') {
-    return next();
-  }
-
-  return res.sendStatus(200);
-});
-
-(async () => {
-  app.get('/leita/:kennitala', async (req, res, next) => {
-    try {
-      const { kennitala } = req.params;
-
-      const data = await getData(kennitala);
-
-      res.json(data);
-    } catch (e) {
-      next(e);
-    }
-  });
-
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
-  let requestId = 1;
-
-  const getData = async kennitala => {
-    const localId = ++requestId;
-
-    console.log(`${localId}: starting`);
-
-    let page = await browser.newPage();
-
-    console.log(`${localId}: goto page`);
-    await page.goto('https://kjorskra.skra.is/kjorskra/', {
-      waitUntil: 'networkidle'
-    });
-    await page.focus('#txtKennitala_I');
-    await page.type('#txtKennitala_I', (kennitala || '').toString(), {
-      delay: 0
-    });
-    await page.click('#btnLeit_CD');
-
-    try {
-      await page.waitFor('#ASPxGridView1_DXDataRow0', { timeout: 2000 });
-    } catch (e) {
-      return {
-        success: false,
-        message: 'Kennitala not found'
-      };
-      page.close();
-    }
-
-    const cells = await page.$$('#ASPxGridView1_DXDataRow0 td');
-
-    const data = await Promise.all(
-      [...cells].map(async rowElement => {
-        const items = await page.evaluate(async el => {
-          const tds = el.querySelectorAll('td');
-          console.log('el', el.textContent.trim());
-          return el.textContent.trim();
-        }, rowElement);
-
-        return items;
-      })
-    );
-
-    page.close();
-
-    console.log(`${localId}: done`);
-
-    return {
-      success: true,
-      kennitala: data[0],
-      nafn: data[1],
-      logheimili: data[2],
-      kjordaemi: data[3],
-      sveitafelag: data[4],
-      kjorstadur: data[5],
-      kjordeild: data[6]
-    };
-  };
-
-  const PORT = process.env.PORT || 3400;
-
-  app.listen(PORT, () => {
+express()
+  .get('/leita/:kennitala', async (req, res, next) => {
+    kjorskra(req.params.kennitala).then(d => res.json(d),next);
+  })
+  .listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
-})();
--- test.js
+++ test.js
@@ -1,44 +1,12 @@
-const puppeteer = require('puppeteer');
+const kjorskra = require('./kjorskra');
 
 (async () => {
   const kennitala = 2804932879;
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
-
   const scrape = async index => {
     console.log(`${index} starting`);
-
-    let page = await browser.newPage();
-
-    console.log(`${index} goto page`);
-    await page.goto('https://kjorskra.skra.is/kjorskra/', {
-      waitUntil: 'networkidle'
-    });
-    await page.focus('#txtKennitala_I');
-    await page.type('#txtKennitala_I', (kennitala || '').toString(), {
-      delay: 0
-    });
-    await page.click('#btnLeit_CD');
-    await page.waitFor('#ASPxGridView1_DXDataRow0', { timeout: 5000 });
-
-    const cells = await page.$$('#ASPxGridView1_DXDataRow0 td');
-
-    const data = await Promise.all(
-      [...cells].map(async rowElement => {
-        const items = await page.evaluate(async el => {
-          const tds = el.querySelectorAll('td');
-          console.log('el', el.textContent.trim());
-          return el.textContent.trim();
-        }, rowElement);
-
-        return items;
-      })
-    );
-
-    console.log(`${index} done`);
+    const result = await kjorskra(kennitala);
+    console.log(`${index} done, success: ${result.success}`);
   };
 
   for (var i = 0; i < 10; i++) {
@@ -49,5 +17,5 @@ const puppeteer = require('puppeteer');
     for (var i = 10; i < 15; i++) {
       scrape(i);
     }
-  }, 2000);
+  }, 1000);
 })();
